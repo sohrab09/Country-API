9587e7764bc56c59109409e8ef0689a704aaaa16
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-02-07 01:55:01 +0600

    Complete project

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Main API Calling
+
 fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     .then(data => displayCountries(data))
@@ -10,8 +12,30 @@ const displayCountries = countries => {
         const countryInfo = `
         <h3 class='country-name'>${country.name}</h3>
         <p>${country.capital}</p>
+        <button onclick="displayCountryDetails('${country.name}')">Details</button>
         `;
         countryDiv.innerHTML = countryInfo
         countriesDiv.appendChild(countryDiv);
     });
+}
+
+// Country Details API Calling 
+
+const displayCountryDetails = name => {
+    const url =`https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]));
+}
+
+// Showing The Country Information 
+
+const renderCountryInfo = country => {
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}">
+    `
 }
